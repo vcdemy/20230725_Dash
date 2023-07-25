35a5618bda9d572d2daf6df1f23ac52ed8c98250
Victor Gau <victorgau@gmail.com>
2023-07-25 16:47:58 +0800

    Update app10.py

--- app10.py
+++ app10.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import pandas as pd
 
 df = pd.read_csv("https://mopsfin.twse.com.tw/opendata/t187ap03_L.csv")
@@ -10,17 +10,17 @@ app.layout = html.Div(
         html.I("請輸入股票代號"),
         html.Br(),
         dcc.Input(id="input1", type="text", placeholder="股票代號"),
-        html.Div(id="output"),
+        dash_table.DataTable(data=None, page_size=10, id="output"),
     ]
 )
 
 
 @callback(
-    Output("output", "children"),
+    Output("output", "data"),
     Input("input1", "value")
 )
 def update_output(input1):
-    return df[df['公司代號'==input1]]
+    return df[df['公司代號'==input1]].to_dict("records")
 
 
 if __name__ == "__main__":
