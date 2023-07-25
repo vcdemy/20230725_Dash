7b8c72c0a55738213fedf26f09c0ec38b6f7ca5b
Victor Gau <victorgau@gmail.com>
2023-07-25 11:57:00 +0800

    Upload app09.py

---
+++ app09.py
@@ -0,0 +1,38 @@
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import yfinance as yf
+
+stocks = ['TSLA', 'MSFT', 'AMZN', 'GOOG', 'AMD', 'NVDA']
+prices = {}
+for stock in stocks:
+    prices[stock] = yf.download(stock)
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(children='股票收盤價線圖'),
+    html.Hr(),
+    html.Div([
+        html.Div(dcc.Dropdown(options=stocks, value='TSLA', id='controls-and-radio-item'), style={'width': '19%', 'display': 'inline-block'}),
+        html.Div(dcc.Slider(10, 100, 10, value=50, id="my-slider"), style={'width': '79%', 'display': 'inline-block'}),
+    ]),
+    html.Hr(),
+    dash_table.DataTable(data=None, page_size=10, id="historical-prices"),
+    dcc.Graph(figure={}, id='controls-and-graph')
+])
+
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Output(component_id="historical-prices", component_property="data"),
+    Input(component_id='controls-and-radio-item', component_property='value'),
+    Input(component_id='my-slider', component_property='value')
+)
+def update_graph(stock, value):
+    fig = px.line(prices[stock].tail(value), x=prices[stock].tail(value).index, y='Close')
+    data = prices[stock].reset_index().tail(value).to_dict('records')
+    return fig, data
+
+if __name__ == '__main__':
+    app.run(debug=True)
