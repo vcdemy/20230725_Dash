4ca408970817d8af38b498d6d304379478515fc1
Victor Gau <victorgau@gmail.com>
2023-07-25 09:37:17 +0800

    Update files

--- app01.py
+++ app01.py
@@ -2,9 +2,7 @@ from dash import Dash, html
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.Div(children='Hello World')
-])
+app.layout = html.H1("Hello Victor")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app03.py
+++ app03.py
@@ -1,12 +1,15 @@
 from dash import Dash, html, dash_table
 import pandas as pd
+import yfinance as yf
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+# TODO
+# 讀取股票歷史資料
+df = yf.download()
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='My First App with Data'),
+    html.Div(children='某某股票歷史股價'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
 
